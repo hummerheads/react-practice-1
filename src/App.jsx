9a1795800865f6ca81a18207aea8f42aee85565b
hummerheads <mdabdullah.allinfozone@gmail.com>
2023-09-18 14:01:02 +0600

    yo

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -25,9 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
